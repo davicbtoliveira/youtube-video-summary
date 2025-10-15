7dd5f538af74eadaaa3e6468fcdf976f4cbf8077
Davi Castello Branco Tavares de Oliveira <davicbtoliveira@hotmail.com>
2025-10-15 00:01:13 -0400

    feat: implementing the gemini integration

---
+++ src/gemini_integration.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from youtube_transcript_api import YouTubeTranscriptApi
+
+import google.generativeai as genai
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=api_key)
+
+def summarize_video(video_url):
+    video_id = video_url.split("v=")[1].split("&")[0]
+    
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    
+    text = " ".join([item['text'] for item in transcript])
+    
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    
+    prompt = f"Summarize this YouTube video transcript: {text}. Be the most concise you can, if you're going to display video times in the summary, please use only MM:SS"
+    
+    response = model.generate_content(prompt)
+    
+    return response.text
